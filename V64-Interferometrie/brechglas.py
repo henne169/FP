57e7fc80e8c0265e02e9c97bb7a0f41f1de8cf73
Nicole Schulte <nicole.schulte@tu.dortmund.de>
2017-11-20 11:55:14 +0100

    .

--- V64-Interferometrie/brechglas.py
+++ V64-Interferometrie/brechglas.py
@@ -1,14 +1,7 @@
-import matplotlib.pyplot as plt
+#Brechunsindizes
 import numpy as np
-from scipy.optimize import curve_fit
-import uncertainties as unc
-import uncertainties.unumpy as unp
-from astropy.io import ascii
-
-#grad, I_min, I_max  = np.genfromtxt('kontrast.txt', unpack=True)
-#K= (I_max - I_min) / (I_max + I_min)
-#Winkel = grad-180
-#ascii.write([np.round(Winkel,0),np.round(I_min,2),np.round(I_max,2),np.round(K,2)], 'tabellekontrast.tex', format='latex')
+from uncertainties import ufloat
+import sympy
 
 Winkela, Anzahl  = np.genfromtxt('glasmessung1.txt', unpack=True)
 Winkelb, Anzahl2  = np.genfromtxt('glasmessung2.txt', unpack=True)
@@ -26,5 +19,9 @@ n1 = (- T * 2* tetha_0 * Winkel) / (Anzahl * lam  - T*(2*tetha_0 *Winkel))
 n2 = (- T * 2* tetha_0 * Winkel2) / (Anzahl2 * lam  - T*(2*tetha_0 *Winkel2))
 n3 = (- T * 2* tetha_0 * Winkel3) / (Anzahl3 * lam  - T*(2*tetha_0 *Winkel3))
 
+n = [n1,n2,n3]
 
-ascii.write([Winkela,Anzahl,np.round(n1,3),Winkelb,Anzahl2,np.round(n2,3),Winkelc,Anzahl3,np.round(n3,3)], 'Glasmessungen2.tex', format='latex')
+m = np.mean(n)
+s = np.std(n)
+print('Mittelwert = ' ,m)
+print('Standartabweichung =', s)
