37adea51b561b74f58c52fcca60aeb22f9986274
unknown <hendrik.boekenkamp@to-dortmund.de>
2017-12-02 14:18:40 +0100

    bla

--- V47-Molwärme/T.py
+++ V47-Molwärme/T.py
@@ -3,24 +3,49 @@ import numpy as np
 from scipy.optimize import curve_fit
 import uncertainties as unc
 import uncertainties.unumpy as unp
+import sympy
 from numpy import linspace, pi,exp, sin, cos, tan, loadtxt, savetxt, mean, zeros, size, log, sum, sqrt, arctan
 from uncertainties import ufloat
 import scipy.stats as st
 from scipy.constants import c, e, h
 from astropy.io import ascii
+################################################################################
+#Umrechnung: Temperatur in Widerstand
 
+#T = np.genfromtxt("T.txt", unpack=True)
+#a = 0.00134
+#b = 2.296
+#c = -243.02
+##T = a*R**2 + b*R + c
+#R = -b/(2*a) + ( (b/(2*a))**2 - ((c-T)/a) )**(1/2)
+#R_1 = R*10**(-3)
+#ascii.write([R_1, T], 'T_1.tex', format='latex')
 
-#plt.rcParams['figure.figsize'] = (10, 8)
-#plt.rcParams['font.size'] = 13
+################################################################################
+#Berechnung der Energien
 
-T = np.genfromtxt("T.txt", unpack=True)
-a = 0.00134
-b = 2.296
-c = -243.02
-#T = a*R**2 + b*R + c
-R = -b/(2*a) + ( (b/(2*a))**2 - ((c-T)/a) )**(1/2)
-R_1 = R*10**(-3)
-ascii.write([R_1, T], 'T_1.tex', format='latex')
+#U,I,t = sympy.var('U,I,t')
+#
+#U = ufloat(19.12, 0.01)
+#I = ufloat(181.5*10**(-3), 0.0001)
+#t = ufloat(361, 5)
+#
+#F =  U*I*t #Funktion
+#
+#print('F = {}'.format(F))
+
+################################################################################
+#Berechnung der Molwärmen bei Konstantem Druck
+E,M,T,m = sympy.var('E,M,T,m')
+
+m = 342
+M = 63.546
+T = ufloat(10, 0.1)
+E = ufloat(1253, 17)
+
+C = (E*M)/(T*m)
+print('C = {}'.format(C))
+################################################################################
 
 #plt.plot(z
 #R 'rx', label='Messdaten')
