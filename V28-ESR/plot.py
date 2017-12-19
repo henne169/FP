5570d1c3b5cd0f99d07150af2a4e964187307577
Nicole Schulte <nicole.schulte@tu.dortmund.de>
2017-12-19 15:19:03 +0100

    gerundet auf 1 signifikante stelle und errorbars

--- V28-ESR/plot.py
+++ V28-ESR/plot.py
@@ -11,6 +11,7 @@ plt.rcParams['font.size'] = 13
 
 nu_e = np.array([10.623,14.732,20.555,23.887,29.391])
 B = np.array([ 308.008,524.924,737.232,830.569,1027.756])
+dB = np.array([34.75,30.09,32.74,26.30,26.54])
 
 def f(nu_e, a, b):
     return a * nu_e + b
@@ -26,8 +27,9 @@ print('b =', params[1], '±', errors[1])
 x_plot = np.linspace(10,35)   #nicht sicher, aber jedes mal beifügen
 
 plt.grid()
-plt.plot(nu_e, B, 'rx', label="mittlere Feldstärken pro Signalfrequenz")
+plt.plot(nu_e, B, 'kx', label="mittlere Feldstärken pro Signalfrequenz")
 plt.plot(x_plot, f(x_plot, *params), 'm-', label='Linear Fit', linewidth=1)
+plt.errorbar(nu_e, B, yerr=dB, fmt='|', ecolor='k', capthick=2)
 plt.legend(loc="best")
 plt.xlabel(r'$\nu_e \, [MHz]$')
 plt.ylabel(r'$B \,[\mu T] $')
